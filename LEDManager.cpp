df5b7acc5bd6b3291cddfd5d602b808537ea19b5
Peter Buelow <goballstate@gmail.com>
2017-01-06 09:27:21 -0600

    Added a demo display pattern for button 3

--- LEDManager.cpp
+++ LEDManager.cpp
@@ -14,7 +14,7 @@ LEDManager::LEDManager(QObject *parent) : QObject(parent)
 
     try {
         // addLeds may throw an exception if the SPI interface cannot be found.
-        FastLED.addLeds<WS2801, 0, 0, RGB>(leds, NUM_LEDS);
+        FastLED.addLeds<WS2801, 0, 0, RGB>(m_leds, NUM_LEDS);
     } catch (std::exception const &exc) {
         std::cerr << "Exception caught " << exc.what() << "\n";
         m_enabled = false;
@@ -22,6 +22,13 @@ LEDManager::LEDManager(QObject *parent) : QObject(parent)
         std::cerr << "Unknown exception caught\n";
         m_enabled = false;
     }
+
+    m_patterns.push_back(std::bind(&LEDManager::rainbow, this));
+    m_patterns.push_back(std::bind(&LEDManager::rainbowWithGlitter, this));
+    m_patterns.push_back(std::bind(&LEDManager::confetti, this));
+    m_patterns.push_back(std::bind(&LEDManager::sinelon, this));
+    m_patterns.push_back(std::bind(&LEDManager::juggle, this));
+    m_patterns.push_back(std::bind(&LEDManager::bpm, this));
 }
 
 LEDManager::~LEDManager()
@@ -45,6 +52,10 @@ void LEDManager::runProgram(int p)
 		m_allowRun = true;
 		snow();
 		break;
+	case 3:
+		m_allowRun = true;
+		demo();
+		break;
 	default:
 		break;
 	}
@@ -64,7 +75,7 @@ void LEDManager::setRGB(uint8_t r, uint8_t g, uint8_t b)
 	c.b = b;
 
 	for (int i =  0; i < NUM_LEDS; i++) {
-		leds[i] = c;
+		m_leds[i] = c;
 	}
 	FastLED.show();
 }
@@ -82,7 +93,7 @@ void LEDManager::turnOff()
 void LEDManager::fadeall()
 {
 	for(int i = 0; i < NUM_LEDS; i++) {
-		leds[i].nscale8(250);
+		m_leds[i].nscale8(250);
 	}
 }
 
@@ -93,7 +104,7 @@ void LEDManager::cylon()
 		// First slide the led in one direction
 		for(int i = 0; i < NUM_LEDS; i++) {
 			// Set the i'th led to red
-			leds[i] = CHSV(hue++, 255, 255);
+			m_leds[i] = CHSV(hue++, 255, 255);
 			// Show the leds
 			FastLED.show();
 			// now that we've shown the leds, reset the i'th led to black
@@ -106,7 +117,7 @@ void LEDManager::cylon()
 		// Now go in the other direction.
 		for(int i = (NUM_LEDS)-1; i >= 0; i--) {
 			// Set the i'th led to red
-			leds[i] = CHSV(hue++, 255, 255);
+			m_leds[i] = CHSV(hue++, 255, 255);
 			// Show the leds
 			FastLED.show();
 			// now that we've shown the leds, reset the i'th led to black
@@ -124,4 +135,88 @@ void LEDManager::cylon()
 void LEDManager::snow()
 {
 
+}
+
+// random colored speckles that blink in and fade smoothly
+void LEDManager::confetti()
+{
+	fadeToBlackBy(m_leds, NUM_LEDS, 10);
+	int pos = random16(NUM_LEDS);
+	m_leds[pos] += CHSV(m_hue + random8(64), 200, 255);
+}
+
+// a colored dot sweeping back and forth, with fading trails
+void LEDManager::sinelon()
+{
+	fadeToBlackBy(m_leds, NUM_LEDS, 20);
+	int pos = beatsin16(13,0,NUM_LEDS-1);
+	m_leds[pos] += CHSV(m_hue, 255, 192);
+}
+
+void LEDManager::bpm()
+{
+	// colored stripes pulsing at a defined Beats-Per-Minute (BPM)
+	uint8_t BeatsPerMinute = 62;
+	CRGBPalette16 palette = PartyColors_p;
+	uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
+
+	for( int i = 0; i < NUM_LEDS; i++) { //9948
+		m_leds[i] = ColorFromPalette(palette, m_hue+(i*2), beat - m_hue+(i*10));
+	}
+}
+
+// eight colored dots, weaving in and out of sync with each other
+void LEDManager::juggle()
+{
+	byte dothue = 0;
+	fadeToBlackBy(m_leds, NUM_LEDS, 20);
+
+	for (int i = 0; i < 8; i++) {
+		m_leds[beatsin16(i+7,0,NUM_LEDS-1)] |= CHSV(dothue, 200, 255);
+		dothue += 32;
+	}
+}
+
+// add one to the current pattern number, and wrap around at the end
+void LEDManager::nextPattern()
+{
+	m_CurrentPatternNumber = (m_CurrentPatternNumber + 1) % m_patterns.size();
+}
+
+// built-in FastLED rainbow, plus some random sparkly glitter
+void LEDManager::rainbowWithGlitter()
+{
+	rainbow();
+	addGlitter(80);
+}
+
+void LEDManager::addGlitter(fract8 chanceOfGlitter)
+{
+	if (random8() < chanceOfGlitter) {
+		m_leds[random16(NUM_LEDS)] += CRGB::White;
+	}
+}
+
+void LEDManager::rainbow()
+{
+  fill_rainbow(m_leds, NUM_LEDS, m_hue, 7);
+}
+
+void LEDManager::demo()
+{
+	while (m_allowRun) {
+		// Call the current pattern function once, updating the 'leds' array
+		m_patterns[m_CurrentPatternNumber]();
+
+		// FastLED.delay will call FastLED.show(), and then delay any
+		// remaining milliseconds
+		FastLED.delay(1000/FRAMES_PER_SECOND);
+
+		// do some periodic updates
+		EVERY_N_MILLISECONDS( 20 ) { m_hue++; } // slowly cycle the "base color" through the rainbow
+		EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+	}
+	FastLED.clear();
+	FastLED.show();
+	emit programDone();
 }
--- LEDManager.h
+++ LEDManager.h
@@ -10,9 +10,18 @@
 
 #include <QtCore/QtCore>
 #include <iostream>
+#include <functional>
 #include <FastLED/FastLED.h>
 
 #define NUM_LEDS	25
+#define SPI_BUS    0
+#define SPI_CS	   0
+
+#define LED_TYPE    		WS2801
+#define COLOR_ORDER 		GRB
+#define BRIGHTNESS          96
+#define FRAMES_PER_SECOND 	120
+
 
 class LEDManager : public QObject {
 	Q_OBJECT
@@ -35,11 +44,25 @@ signals:
 private:
 	void cylon();
 	void snow();
+	void demo();
 	void fadeall();
+	void rainbow();
+	void addGlitter(fract8);
+	void rainbowWithGlitter();
+	void nextPattern();
+	void juggle();
+	void bpm();
+	void sinelon();
+	void confetti();
 
-	CRGB leds[NUM_LEDS];
+	CRGB m_leds[NUM_LEDS];
 	bool m_enabled;
 	bool m_allowRun;
+
+	uint8_t m_CurrentPatternNumber = 0; // Index number of which pattern is current
+	uint8_t m_hue = 0; 					// rotating "base color" used by many of the patterns
+
+	QVector< std::function<void(void)> >m_patterns;
 };
 
 #endif /* LIGHTSMANAGER_H_ */
